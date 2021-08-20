45660cb26728a9e66b79b6a810d23edb141dad2e
moononournation <moononournation@gmail.com>
2021-08-20 13:02:28 +0800

    update find photoId method

--- app/lib/googlePhoto.py
+++ app/lib/googlePhoto.py
@@ -9,18 +9,14 @@ GOOGLEPHOTOSEARCHPATTERN = "\",[\"" + GOOGLEPHOTOURLPREFIX
 
 def updatePhoto(PHOTOPATH):
     r = requests.get(GOOGLEPHOTOURL, allow_redirects=True)
-    seekPatternFound = False
 
     for iterLine in r.iter_lines():
         if iterLine:
             line = iterLine.decode('utf-8')
-            if not seekPatternFound:
-                pos = line.find(GOOGLEPHOTOSEEKPATTERN)
-                if pos > 0: seekPatternFound = True
-            else:
-                GOOGLEPHOTOSEARCHPATTERN
-                pos1 = line.find(GOOGLEPHOTOSEARCHPATTERN)
-                if pos1 > 0:
+            pos = line.find(GOOGLEPHOTOSEEKPATTERN)
+            if pos > 0:
+                pos1 = line.find(GOOGLEPHOTOSEARCHPATTERN, pos)
+                while pos1 > 0:
                     pos1 += len(GOOGLEPHOTOSEARCHPATTERN)
                     pos2 = line.find("\"", pos1)
                     if pos2 > 0:
@@ -30,6 +26,8 @@ def updatePhoto(PHOTOPATH):
                             print(photoFilename)
                         if not os.path.exists(photoFilename):
                             photoUrl = GOOGLEPHOTOURLPREFIX + photoId + "=w1920"
+                            # print(photoUrl)
                             photoRequest = requests.get(photoUrl, allow_redirects=True)
                             open(photoFilename, 'wb').write(photoRequest.content)
                             photoRequest.close()
+                    pos1 = line.find(GOOGLEPHOTOSEARCHPATTERN, pos2)
